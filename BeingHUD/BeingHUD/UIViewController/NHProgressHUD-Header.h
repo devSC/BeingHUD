173306690e7ecaa07b203cae51f4b2bc38cc387f
Wilson Yuan <yuansc@nihao.com>
2015-11-13 19:31:48 +0800

    Update

--- BeingHUD/BeingHUD/UIViewController/NHProgressHUD-Header.h
+++ BeingHUD/BeingHUD/UIViewController/NHProgressHUD-Header.h
@@ -9,5 +9,25 @@
 #ifndef NHProgressHUD_Header_h
 #define NHProgressHUD_Header_h
 
+typedef NS_ENUM(NSInteger, NHHUDDeterminateMode) {
+    /** Progress is shown using a round, pie-chart like, progress view. */
+    NHHUDDeterminateModeDefault,
+    /** Progress is shown using a horizontal progress bar */
+    NHHUDDeterminateModeHorizontalBar,
+    /** Progress is shown using a ring-shaped progress view. */
+    NHHUDDeterminateModeAnnular,
+};
 
+
+
+typedef NS_ENUM(NSInteger, NHHUDViewMode) {
+    /** Progress is shown using an UIActivityIndicatorView. This is the default. */
+    NHHUDViewModeIndeterminate,
+    /** Progress is shown using progress view. */
+    NHHUDViewModeDeterminate,
+    /** Shows a custom view */
+    NHHUDViewModeCustomView,
+    /** Shows only labels */
+    NHHUDViewModeText,
+};
 #endif /* NHProgressHUD_Header_h */
